daaad7b9cc5a2b40f1cb4cd394294445d52814bf
Eric Richard <reric0802@gmail.com>
2024-11-11 13:58:58 +0300

    adds home and about page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Home from './home'
+import About from './About'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home />
+    <About />
   </StrictMode>,
 )
