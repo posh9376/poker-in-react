f1948d2942a7e229c1a365529dcaaaa12b0a78c5
Abdirahman005 <abdirahman.abdullah@student.moringaschool.com>
2024-11-13 09:54:27 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import GameSide from './Compontents/gameSide'
+import Game from './Compontents/explain'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GameSide />
+    <Game/>
   </StrictMode>,
 )
---
+++ src/me.jsx
@@ -0,0 +1,10 @@
+import img2 from "../assets/Velvet And Low-Backed Chair2.jpeg"
+
+
+function Me() {
+    return <div className="player">
+        <img src={img2} alt="" />
+    </div>;
+}
+
+export default Me
