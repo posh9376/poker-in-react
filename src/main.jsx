83348130bd3a1f3bf15db5cdf5c42a61296e5da4
bscit-05-39008695 <aronmutai99@gmail.com>
2024-11-13 15:27:30 +0300

    step1.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import About from './About'
 import GamePage from './gamePage'
 import Navbar from './navbar'
 import Step1 from './step1'
+import Step2 from './step2'
 
 
 createRoot(document.getElementById('root')).render(
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')).render(
     <Home />
     <About />
     <Step1 />
+    <Step2/>
     <GamePage />
   </StrictMode>,
 )
